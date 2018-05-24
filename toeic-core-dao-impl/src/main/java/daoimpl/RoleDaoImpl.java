7dc2829ef7452c7b73fd8c31e8554d191ec2c4a0
Nhật Minh Nguyễn Đặng <nhatminh.congnghephanmem@gmail.com>
2018-05-25 00:30:13 +0700

    delete all

--- toeic-core-dao-impl/src/main/java/daoimpl/RoleDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package daoimpl;
-
-import english.online.core.dao.RoleDao;
-import english.online.core.data.daoimpl.AbstractDao;
-import english.online.core.persistence.entity.RoleEntity;
-
-public class RoleDaoImpl extends AbstractDao<Integer,RoleEntity> implements RoleDao {
-}
